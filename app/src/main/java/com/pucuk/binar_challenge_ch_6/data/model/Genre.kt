9e711bf6fac19b80b951bdc1aaa4fffc5ba9c8fe
Pucukio <pucuk4us@gmail.com>
2023-05-18 13:42:02 +0700

    Set API using Retrofit

---
+++ app/src/main/java/com/pucuk/binar_challenge_ch_6/data/model/Genre.kt
@@ -0,0 +1,11 @@
+package com.pucuk.binar_challenge_ch_6.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
---
+++ app/src/main/java/com/pucuk/binar_challenge_ch_6/data/network/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.pucuk.binar_challenge_ch_6.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    const val  BASE_URL ="https://api.themoviedb.org/3/"
+
+    val instance : ApiClient by lazy {
+        val retrofit= Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(ApiClient::class.java)
+    }
+}
